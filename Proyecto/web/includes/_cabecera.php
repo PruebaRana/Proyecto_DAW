47db20a1c963913c3d0e255f770c6a0c2c0fc53d
PruebaRana <jaguila@pymsolutions.com>
2016-05-29 14:20:45 +0200

    Adaptados los estilos para menus y submenus

--- Proyecto/web/includes/_cabecera.php
+++ Proyecto/web/includes/_cabecera.php
@@ -15,10 +15,19 @@ if ($usuario->Id == 0) {
 }else{
 	?>
 	<nav>
-		<ul id="menus">
+		<ul id="menu">
 			<li><a href="./index.php?controlador=Inicio&accion=indice"><i class="fa fa-home"></i></a></li>
 			<?php if ($usuario->isInRol("Administrador")) { ?>
 				<li class="menuactual"><a href="./index.php?controlador=Usuario&accion=indice">Usuarios</a></li>
+				<li><a href="#">Auxiliares <i class="fa fa-caret-down"></i></a>
+					<ul >
+						<li><a href="./index.php?controlador=Ciclo&accion=indice">Ciclos</a></li>
+						<li><a href="./index.php?controlador=Grupo&accion=indice">Grupos</a></li>
+						<li><a href="./index.php?controlador=Modulo&accion=indice">Modulos</a></li>
+						<li><a href="./index.php?controlador=Cualidad&accion=indice">Cualidades</a></li>
+						<li><a href="./index.php?controlador=Valoracion&accion=indice">Valoraciones</a></li>
+					</ul>
+				</li>
 			<?php } else if ($usuario->isInRol("Profesor")) { ?>
 				<li><a href="./index.php?controlador=Usuario&accion=indice">Alumnos</a></li>
 			<?php } ?>
